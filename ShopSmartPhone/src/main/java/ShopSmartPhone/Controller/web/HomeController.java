9e53240caad88d3cb0f58e03507ac4fe138e9f0b
hoangkhanh7030 <hoangkhanh7030@gmail.com>
2021-05-04 11:59:50 +0700

    create project

--- ShopSmartPhone/src/main/java/ShopSmartPhone/Controller/web/HomeController.java
+++ ShopSmartPhone/src/main/java/ShopSmartPhone/Controller/web/HomeController.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(urlPatterns = {"/trang-chu"})
+@WebServlet(urlPatterns = {"/trang-chu","/login"})
 public class HomeController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -17,7 +17,7 @@ public class HomeController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        RequestDispatcher requestDispatcher=request.getRequestDispatcher("/views/web/home.jsp");
+        RequestDispatcher requestDispatcher=request.getRequestDispatcher("/views/login.jsp");
         requestDispatcher.forward(request,response);
     }
 }
